f91d29fc2b4ce43922a3e8943c7fb6a5274f1e72
Sumit_Devops <sumit_devops@Sumit-Devopss-MacBook-Pro.local>
2023-07-03 00:41:42 +0530

    webhook

--- contact_process.php
+++ contact_process.php
@@ -1,51 +1,31 @@
 <?php
-    // Your Slack API token
-    $slackToken = 'MYSLACK TOKEN';
 
-    // Validate the form fields
-    $name = $_REQUEST['message'];
-    $email = $_REQUEST['name'];
-    $subject = $_REQUEST['email'];
-    $message = $_REQUEST['subject'];
+$webhookUrl = "https://hooks.slack.com/services/your-webhook-url";
 
-    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
-        // Handle the case when one or more fields are missing
-        die("Please fill out all the required fields.");
-    }
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+$message = $_REQUEST['message'];
 
-    // Construct the Slack message
-    $slackMessage = "New contact form submission:\n\n";
-    $slackMessage .= "Name: " . $name . "\n";
-    $slackMessage .= "Email: " . $email . "\n";
-    $slackMessage .= "Subject: " . $subject . "\n";
-    $slackMessage .= "Message: " . $message . "\n";
+$data = array(
+    'text' => "You have a new message from your website:\n\nName: $name\nEmail: $email\nSubject: $subject\nMessage: $message"
+);
 
-    // Slack API endpoint for posting messages
-    $slackApiUrl = 'https://slack.com/api/chat.postMessage';
-// 
-    $data = array(
-        'token' => $slackToken,
-        'channel' => 'C053HJUK24C', // Replace with the ID of the channel you want to post the message to
-        'text' => $slackMessage
-    );
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json",
+        'method'  => 'POST',
+        'content' => json_encode($data)
+    )
+);
 
-    // Use cURL to make the API call
-    $ch = curl_init($slackApiUrl);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$context  = stream_context_create($options);
+$result = file_get_contents($webhookUrl, false, $context);
 
-    $result = curl_exec($ch);
-    curl_close($ch);
+if ($result === false) {
+    // Error handling if the request fails
+} else {
+    // Success handling if the request is successful
+}
 
-    $response = json_decode($result, true);
-
-    // Optionally, you can check the response and handle any errors.
-    if (!$response['ok']) {
-        // Error handling
-        die("Failed to send the Slack notification.");
-    } else {
-        // Success handling
-        echo "Slack notification sent successfully.";
-    }
 ?>
