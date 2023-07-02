320d18248a114ddc755255e4d6f01de9d6cf5adc
Sumit_Devops <sumit_devops@Sumit-Devopss-MacBook-Pro.local>
2023-07-03 00:53:11 +0530

    contact form

--- contact_process.php
+++ contact_process.php
@@ -3,12 +3,25 @@
 $webhookUrl = "https://hooks.slack.com/services/your-webhook-url";
 
 $name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
+//$email = $_REQUEST['email'];
 $subject = $_REQUEST['subject'];
 $message = $_REQUEST['message'];
+$number = $_REQUEST['number'];
+
+// Validate mandatory fields
+if (empty($name) || empty($subject) || empty($message) || empty($number)) {
+    echo "Please fill in all the required fields.";
+    exit;
+}
+
+// Validate contact number
+if (!preg_match('/^\d{10}$/', $number)) {
+    echo "Please enter a 10-digit contact number.";
+    exit;
+}
 
 $data = array(
-    'text' => "You have a new message from your website:\n\nName: $name\nEmail: $email\nSubject: $subject\nMessage: $message"
+    'text' => "You have a new message from your website:\n\nName: $name\nContact: $number\nSubject: $subject\nMessage: $message"
 );
 
 $options = array(
@@ -23,9 +36,9 @@ $context  = stream_context_create($options);
 $result = file_get_contents($webhookUrl, false, $context);
 
 if ($result === false) {
-    // Error handling if the request fails
+    echo "An error occurred while sending the message.";
 } else {
-    // Success handling if the request is successful
+    echo "Your message was sent successfully. We will connect with you shortly.";
 }
 
 ?>
